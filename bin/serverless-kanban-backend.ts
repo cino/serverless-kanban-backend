b8158e16991d7726067cecb17fb5471f4b271ae4
Ricardo Cino <ricardo@cino.io>
2022-04-22 21:08:32 +0200

    Add verified SES domain for future and modfy Cognito UserPool

--- bin/serverless-kanban-backend.ts
+++ bin/serverless-kanban-backend.ts
@@ -7,6 +7,8 @@ const app = new cdk.App();
 new ServerlessKanbanBackendStack(app, 'ServerlessKanbanBackendStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION
+    region: process.env.CDK_DEFAULT_REGION,
   },
+
+  rootDomain: 'cino.io',
 });
--- lib/serverless-kanban-backend-stack.ts
+++ lib/serverless-kanban-backend-stack.ts
@@ -1,10 +1,23 @@
 import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as cognito from 'aws-cdk-lib/aws-cognito';
+import { BooleanAttribute, Mfa, UserPoolEmail } from 'aws-cdk-lib/aws-cognito';
+import { VerifySesDomain } from '@seeebiii/ses-verify-identities';
+
+interface ServerlessKanbanStackProps extends StackProps {
+  rootDomain: string;
+}
 
 export class ServerlessKanbanBackendStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: ServerlessKanbanStackProps) {
     super(scope, id, props);
+
+    // Verify SES Domain
+    new VerifySesDomain(this, 'VerifySesDomain', {
+      domainName: 'kanban.cino.io',
+      hostedZoneName: props.rootDomain,
+      addMxRecord: false,
+    });
 
     const userPool = new cognito.UserPool(this, 'ServerlessKanbanUserPool', {
       signInAliases: {
@@ -12,10 +25,31 @@ export class ServerlessKanbanBackendStack extends Stack {
         username: true,
       },
       selfSignUpEnabled: true,
-      userVerification: {
-        emailStyle: cognito.VerificationEmailStyle.CODE,
+      // auto verify for now.
+      autoVerify: {
+        email: true,
       },
-      // TODO: Add SES email
+
+      // TODO: Re-Look at this once https://github.com/aws/aws-cdk/pull/19790is merged and released
+      // email: UserPoolEmail.withSES({
+      //   replyTo: 'no-reply@kanban.cino.io',
+      //   fromName: 'Serverless Kanban',
+      // }),
+      email: UserPoolEmail.withCognito('no-reply@kanban.cino.io'),
+
+      // Multi-Factor is optionally available and only by a On-Time-Password as SMS
+      // is considered insecure and I'm not paying for that.
+      mfa: Mfa.OPTIONAL,
+      mfaSecondFactor: {
+        sms: false,
+        otp: true,
+      },
+
+      // Extra attributes in Cognito to store Email preferences
+      customAttributes: {
+        notifyItemUpdated: new BooleanAttribute({ mutable: true }),
+        notifyMentioned: new BooleanAttribute({ mutable: true }),
+      },
     });
     const client = userPool.addClient('ServerlessKanbanWebClient');
 
